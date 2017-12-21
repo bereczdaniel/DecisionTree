99e95a8bbdf82ce534e31e6d6cd65054d55bbdd9
Berecz Daniel <berecz.17@gmail.com>
2017-12-21 12:09:03 +0100

    gini implementation for maps - unify later

--- src/main/scala/Purity/Measures/Gini.scala
+++ src/main/scala/Purity/Measures/Gini.scala
@@ -10,4 +10,10 @@ object Gini {
     1.0 - fractions.sum
   }
 
+  def gini(classCounters: Map[String, Int]): Double = {
+    val allInstance = classCounters.values.sum.toDouble
+    val fractions = classCounters.map(x => math.pow(x._2 / allInstance,2))
+    1.0 - fractions.sum
+  }
+
 }
